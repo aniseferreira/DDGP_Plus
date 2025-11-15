94a27231c0a117585a1b5042b39f83ce6e9d4982
Anise D'Orange Ferreira <aniseferreira@users.noreply.github.com>
2025-11-15 19:17:57 -0300

    Create __init__.py
    nao sei para que serve

---
+++ ddgp/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Pacote DDGP — Módulos de suporte ao aplicativo DDGP_plus.
+
+Contém:
+- utils.py     (funções auxiliares, normalização)
+- morph.py     (análise morfológica simples)
+- lexicon.py   (carregamento e lookup do léxico DDGP 3.x)
+"""
+
+__all__ = ["utils", "morph", "lexicon"]

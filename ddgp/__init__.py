bc8f65d5086d5de87ec316c8887ba8033a7ea788
Anise D'Orange Ferreira <aniseferreira@users.noreply.github.com>
2025-11-23 20:48:19 -0300

    Update __init__.py

--- ddgp/__init__.py
+++ ddgp/__init__.py
@@ -1,13 +1,21 @@
 # -*- coding: utf-8 -*-
 """
 Pacote DDGP — módulos de suporte ao aplicativo DDGP_plus.
+
+Contém:
+- utils.py
+- morph_simple.py
+- morph_morpheus.py
+- lexicon.py
+- translit.py
+- formatting.py
 """
 
 __all__ = [
     "utils",
-    "formatting",
-    "lexicon",
-    "translit",
     "morph_simple",
     "morph_morpheus",
+    "lexicon",
+    "translit",
+    "formatting"
 ]
